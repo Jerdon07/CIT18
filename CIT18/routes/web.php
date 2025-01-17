e9e317731cba94b851f8d850bff0f317f9e10036
Jerdon Litaoen <156732894+Jerdon07@users.noreply.github.com>
2025-01-17 04:43:47 +0000

    just another day

--- CIT18/routes/web.php
+++ CIT18/routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\index;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GreetControler;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [index::class, ])
 
 Route::get('/greeting', function () {
     return view('greet');
