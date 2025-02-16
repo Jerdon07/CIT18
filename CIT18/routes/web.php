d62142f15a1de5f5fba87701387fb28b8e597c9f
Jerdon Litaoen <jdlitaoen@gmail.com>
2025-02-16 13:09:08 +0800

    Updated the Code

--- CIT18/routes/web.php
+++ CIT18/routes/web.php
@@ -3,15 +3,25 @@
 use App\Http\Controllers\index;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GreetControler;
+use App\Http\Controllers\tasksController;
+use App\Http\Controllers\ItemController;
 
+// Default Laravel Welcome Page
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/greeting', function () {
-    return view('greet');
+// ACTIVITY: SIMPLE ROUTING TO VIEWS
+// Simply Returns a message
+Route::get('/hello', function () {
+    return 'Hello, Laravel!';
 });
-
+// Calling GreetControler's hello method
 Route::get('/hello', [GreetControler::class, 'hello']);
 
+
+
 Route::resource('tasks', tasksController::class);
+
+Route::get('/items', [ItemController::class, 'index']);
+Route::get('/items/{id}', [ItemController::class, 'show']);
