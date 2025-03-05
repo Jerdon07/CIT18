4ff4c475ac0cc139b146045963d5cefd05aa5bfd
Jerdon Litaoen <156732894+Jerdon07@users.noreply.github.com>
2025-03-05 14:24:00 +0000

    Installed Tailwindcss

--- CIT18/resources/views/layout/layout.blade.php
+++ CIT18/resources/views/layout/layout.blade.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('respurces/css/app.css')
-
+    @vite('resources/css/app.css')
     <title>CIT18</title>
 </head>
 <body>
